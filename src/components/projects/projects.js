7cd8d62802a099c7c70bfe8915155904f903c8d1
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-08-03 08:44:10 -0300

    criei e fiz o design do componente de projetos

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from './projects.module.css';
-
+import { Link } from "react-router-dom";
 
 function Projects () {
 
@@ -9,6 +9,20 @@ function Projects () {
             <div className={styles.title}>
                 <h2>Projetos</h2>
             </div>
+
+            <section className={styles.projectsWindow}>
+                <h3>Title</h3>
+                <Link className={styles.button}>
+                    Ver mais
+                </Link>
+            </section>
+
+            <section className={styles.projectsWindow}>
+                <h3>Title</h3>
+                <Link className={styles.button}>
+                    Ver mais
+                </Link>
+            </section>
         </div>
     )
 }   
