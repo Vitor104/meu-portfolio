b1a8a126bcbadd7ba09758aa4e2bfa1390de75cf
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-08-06 06:35:27 -0300

    praticamente finalizado

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -11,15 +11,29 @@ function Projects () {
             </div>
 
             <section className={styles.projectsWindow}>
-                <h3>Title</h3>
-                <Link className={styles.button}>
+                <h3>SunSplash Haven</h3>
+                <Link to="https://vitor104.github.io/catalogo-de-produtos/index.html" className={styles.button}>
                     Ver mais
                 </Link>
             </section>
 
             <section className={styles.projectsWindow}>
-                <h3>Title</h3>
-                <Link className={styles.button}>
+                <h3>Calculator</h3>
+                <Link to="https://vitor104.github.io/calculator/" className={styles.button}>
+                    Ver mais
+                </Link>
+            </section>
+
+            <section className={styles.projectsWindow}>
+                <h3>Jammming</h3>
+                <Link to="https://reactmusicappjam.netlify.app/" className={styles.button}>
+                    Ver mais
+                </Link>
+            </section>
+
+            <section className={styles.projectsWindow}>
+                <h3>Sky Cast</h3>
+                <Link to="https://skycastdaily.netlify.app/" className={styles.button}>
                     Ver mais
                 </Link>
             </section>
